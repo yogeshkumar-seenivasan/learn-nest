df10e6a6cdada6c0b0100f39a8b4229f411ba31c
Yogesh Kumar <yogeshkumarseenivasan@gmail.com>
2022-11-20 01:52:27 +0530

    - First project runnable code

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import {Controller, Get, Module} from "@nestjs/common";
+import {NestFactory} from "@nestjs/core";
+
+@Controller()
+class AppController {
+
+    @Get()
+    getRootRoute() {
+        return 'first API';
+    }
+}
+
+
+@Module({
+    controllers: [AppController]
+})
+class AppModule {
+
+}
+
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+}
+
+bootstrap();
